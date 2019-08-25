ee532a272bbafc8e6a7a6274fa12c5a0bf112442
Sachin Shetty <NONE>
2019-08-25 10:49:55 +0200

    index.html : updated footer and logo

--- js/main.js
+++ js/main.js
@@ -8,6 +8,13 @@ function w3_close() {
   document.getElementById("myOverlay").style.display = "none";
 }
 function myAccFunc1() {
+
+/*  var para = document.createElement("sideTagsFlavor");
+  para.innerHTML = "<a href=\"#\" class=\"w3-bar-item w3-button w3-bar-block w3-hide w3-padding-large w3-medium w3-show\">Caramel ssCharm</a> ";
+  document.getElementById("generated").appendChild(para);
+*/
+  //Use recplace Child for handling data
+
   var x = document.getElementById("demoAcc1");
   if (x.className.indexOf("w3-show") == -1) {
     x.className += " w3-show";
@@ -32,4 +39,10 @@ function myAccFunc3() {
   } else {
     x.className = x.className.replace(" w3-show", "");
   }
+
+function sideTags() {
+  var para = document.createElement("sideTagsFlavor");
+  para.innerHTML = "<a href=\"#\" class=\"w3-bar-item w3-button\">Caramel Charm</a> <a href=\"#\" class=\"w3-bar-item w3-button\">Chilli Chocolate</a> <a href=\"#\" class=\"w3-bar-item w3-button\">Dark Almond Delight</a>";
+  document.getElementById("choc_flavors").appendChild(para);
+}
 }
