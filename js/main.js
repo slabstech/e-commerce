e150804e66e50507b7db45ae25fbe2df2c94b2ce
Sachin Shetty <NONE>
2019-08-25 19:01:43 +0200

    sidebar generated from json

--- js/main.js
+++ js/main.js
@@ -7,13 +7,44 @@ function w3_close() {
   document.getElementById("mySidebar").style.display = "none";
   document.getElementById("myOverlay").style.display = "none";
 }
-function myAccFunc1() {
 
-/*  var para = document.createElement("sideTagsFlavor");
-  para.innerHTML = "<a href=\"#\" class=\"w3-bar-item w3-button w3-bar-block w3-hide w3-padding-large w3-medium w3-show\">Caramel ssCharm</a> ";
+function loadSideBar(){
+
+  var para = document.createElement("sideTags");
+  var para_2 = document.createElement("sideTags2");
+
+  var flavors = [ "Caramel Charm", "Chilli Chocolate", "Dark Almond Delight",  "Dark Almond Delight Square", "Date Exotica", "Milk Almond Delight", "Milk Almond Delight Square", "White Almond Delight", "White Almond Delight Square"];
+  var packs = [ "Draw Box 12", "Gold Box", "Celebration Box", "Window Box" ] ;
+
+  var text = "";
+  var text_2 = "";
+  var x,x2;
+
+  for (x in flavors) {
+    text += "<a href=\"#\" class=\"w3-bar-item w3-button w3-bar-block w3-hide w3-padding-large w3-medium w3-show\">" ;
+    text += flavors[x];
+    text +="</a> " ;
+  }
+
+  for (x2 in packs) {
+    text_2 += "<a href=\"#\" class=\"w3-bar-item w3-button w3-bar-block w3-hide w3-padding-large w3-medium w3-show\">" ;
+    text_2 += packs[x2];
+    text_2 +="</a> " ;
+  }
+
+  para.innerHTML = text;
   document.getElementById("generated").appendChild(para);
-*/
-  //Use recplace Child for handling data
+
+  para_2.innerHTML = text_2;
+  document.getElementById("generated_2").appendChild(para_2);
+
+}
+
+function onSiteLoad(){
+    loadSideBar();
+}
+
+function myAccFunc1() {
 
   var x = document.getElementById("demoAcc1");
   if (x.className.indexOf("w3-show") == -1) {
@@ -30,19 +61,4 @@ function myAccFunc2() {
   } else {
     x.className = x.className.replace(" w3-show", "");
   }
-}
-
-function myAccFunc3() {
-  var x = document.getElementById("demoAcc3");
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-  } else {
-    x.className = x.className.replace(" w3-show", "");
-  }
-
-function sideTags() {
-  var para = document.createElement("sideTagsFlavor");
-  para.innerHTML = "<a href=\"#\" class=\"w3-bar-item w3-button\">Caramel Charm</a> <a href=\"#\" class=\"w3-bar-item w3-button\">Chilli Chocolate</a> <a href=\"#\" class=\"w3-bar-item w3-button\">Dark Almond Delight</a>";
-  document.getElementById("choc_flavors").appendChild(para);
-}
 }
