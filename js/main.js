881be060b2fd9ae43a62172997df1b96057f8440
Sachin Shetty <NONE>
2019-08-26 08:50:33 +0200

    product information generated from list

--- js/main.js
+++ js/main.js
@@ -20,16 +20,19 @@ function loadSideBar(){
   var text_2 = "";
   var x,x2;
 
+  var anchorStart = "<a href=\"#\" class=\"w3-bar-item w3-button w3-bar-block w3-hide w3-padding-large w3-medium w3-show\">" ;
+  var anchorEnd = "</a> " ;
+
   for (x in flavors) {
-    text += "<a href=\"#\" class=\"w3-bar-item w3-button w3-bar-block w3-hide w3-padding-large w3-medium w3-show\">" ;
+    text += anchorStart ;
     text += flavors[x];
-    text +="</a> " ;
+    text += anchorEnd ;
   }
 
   for (x2 in packs) {
-    text_2 += "<a href=\"#\" class=\"w3-bar-item w3-button w3-bar-block w3-hide w3-padding-large w3-medium w3-show\">" ;
+    text_2 += anchorStart ;
     text_2 += packs[x2];
-    text_2 +="</a> " ;
+    text_2 += anchorEnd ;
   }
 
   para.innerHTML = text;
@@ -37,25 +40,85 @@ function loadSideBar(){
 
   para_2.innerHTML = text_2;
   document.getElementById("generated_2").appendChild(para_2);
+}
+
+function loadProducts(){
+
+  var para = document.createElement("sideProducts");
+
+  var i=0;
+  var product_image = [ "choc_flavors/date_exotica.jpg", "choc_flavors/chilli_chocolate.jpg", "choc_flavors/caramel_charm.jpg", "choc_flavors/white_almond_delight.jpg", "choc_packs/celebration.jpg", "choc_packs/tin_box.jpg", "choc_packs/window_box.jpg", "choc_packs/draw_box_12.jpg"];
+  var product_name = ["Date Exotica", "Chilli Chocolate", "Caramel Charm", "White Almond Delight", "Celebration Pack", "Design Box", "Window Box", "Draw Box : 12"] ;
+  var product_cost = ["$19.99", "$19.99", "$24.99", "$20.50" , "$20.50", "$14.99" , "$14.99", "$24.99"];
+  var text = "";
+  var text_2 = "";
+  var x,x2;
+
+
+
+  var folderPath = "w3images/products/"
+
+  var columndiv = "<div class=\"w3-col l3 s6\">";
+  var anchorStart = "<div class=\"w3-container\"> <img style=\"width:100%\" src=\"" ;
+
+
+  var anchr2 = "\"><p>";
+
+  var anchr3 = "<br><b>" ;
+
+  var anchrLast = "</b></p> </div>";
+
+  var columndivEnd = "</div>";
+
+  // TODO additional hover code
+  /*
+    var anchorStartAdd = " <div class=\"w3-display-container\" >" ;
+
+  var anchorEnd = "\"> <span class=\"w3-tag w3-display-topleft\">New</span> <div class=\"w3-display-middle w3-display-hover\"> " ;
+
+  var anchr2Add = "<button class=\"w3-button w3-black\">Buy now <i class=\"fa fa-shopping-cart\"></i></button> </div> </div> ";
+  */
+
+  for (x in product_image) {
+    if(i%2 == 0)
+    {
+      text += columndiv ;
+    }
+    text += anchorStart ;
+    text += folderPath ;
+
+    text += product_image[x];
+    text += anchr2 ;
+
+    text += product_name[x] ;
+
+    text += anchr3 ;
+
+    text += product_cost[x];
+    text += anchrLast ;
+
+    if(i%2 != 0)
+    {
+      text += columndivEnd ;
+    }
+
+    i++ ;
+  }
+
+  para.innerHTML = text;
+  document.getElementById("generated_products").appendChild(para);
 
 }
 
 function onSiteLoad(){
     loadSideBar();
+    loadProducts();
 }
 
-function myAccFunc1() {
+function displaySideBar(id) {
 
-  var x = document.getElementById("demoAcc1");
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-  } else {
-    x.className = x.className.replace(" w3-show", "");
-  }
-}
-
-function myAccFunc2() {
-  var x = document.getElementById("demoAcc2");
+  var tag = "demoAcc" + id ;
+  var x = document.getElementById(tag);
   if (x.className.indexOf("w3-show") == -1) {
     x.className += " w3-show";
   } else {
